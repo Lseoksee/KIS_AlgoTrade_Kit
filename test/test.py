66c3038fbd4fa24ee2ed7da0814352698717782a
LOUK342 <kimjaeho342@gmail.com>
2024-03-18 00:36:00 +0900

    .

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 from pykis import *
 from dotenv import dotenv_values
 
-env_vars = dotenv_values('info.env')
+env_vars = dotenv_values('./test/info.env')
 
 kis = PyKis(
     appkey=env_vars.get('appKey'),
@@ -10,7 +10,10 @@ kis = PyKis(
 )
 
 # 계좌연결
-account = kis.account(env_vars.get('secretKey'))
+account = kis.account(env_vars.get('bankAccount'))
 # 잔고조회
 balance = account.balance_all()
 print(f'예수금: {balance.dnca_tot_amt:,}원 평가금: {balance.tot_evlu_amt:,} 손익: {balance.evlu_pfls_smtl_amt:,}원', flush=True)
+
+stock = kis.market.stock('375500')
+print(stock.mksc_shrn_iscd, stock.hts_kor_isnm)
