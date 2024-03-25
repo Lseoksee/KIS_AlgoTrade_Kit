681d318bd17dc2839b5a3c6828ca8774160bf222
LOUK342 <kimjaeho342@gmail.com>
2024-03-25 15:57:13 +0900

    1

--- test/PIC.py
+++ test/PIC.py
@@ -2,7 +2,7 @@ import sys
 from pykis import *
 from dotenv import dotenv_values
 
-env_vars = dotenv_values('info.env')
+env_vars = dotenv_values('./test/info.env')
 
 kis = PyKis(
     appkey=env_vars.get('appKey'),
@@ -11,7 +11,7 @@ kis = PyKis(
 )
 
 # 계좌연결
-account = kis.account(env_vars.get('secretKey'))
+account = kis.account(env_vars.get('bankAccount'))
 # 잔고조회
 balance = account.balance_all()
 print(f'예수금: {balance.dnca_tot_amt:,}원 평가금: {balance.tot_evlu_amt:,} 손익: {balance.evlu_pfls_smtl_amt:,}원', flush=True)
