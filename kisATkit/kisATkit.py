bab7b131d1db4c84cc596f5925b78ac6b4615b6b
Lseoksee <da864268@naver.com>
2024-03-27 20:43:51 +0900

    멀티쓰레딩 토큰 재갱신 추가

--- kisATkit/kisATkit.py
+++ kisATkit/kisATkit.py
@@ -1,14 +1,16 @@
+from threading import Thread
 from typing import Literal
 import requests
-from threading import Thread
 import time
 from kisATkit import Constant
 
 class KisATkit:
+    tokenUrl = "/oauth2/tokenP"
+    
     def __init__(self, type: Literal["실전", "모의"], appKey: str, appSecret: str, account: str, HTS_Id: str):
         """
         한국 투자증권 API 를 사용하기 위한 토큰 키 발급 및 기본정보를 입력합니다.\n
-        토큰 재발급 처리는 만료 24분전에 자동으로 갱신되니 따로 처리하실 필요가 없습니다.
+        토큰 재발급 처리는 만료 27분전에 자동으로 갱신되니 따로 처리하실 필요가 없습니다.
 
         Args:
             type ('실전', '모의'): 실전투자인지 모의 투자 인지
@@ -18,8 +20,6 @@ class KisATkit:
             HTS_Id (str): 한국투자증권 HTS ID
         """
 
-        url = "/oauth2/tokenP"
-
         reqJson = {
             "grant_type": "client_credentials",
             "appkey": appKey,
@@ -27,52 +27,51 @@ class KisATkit:
         }
 
         req = requests.post(
-            "{}{}".format(Constant.REAL_URL if type == "실전" else Constant.TEST_URL, url), json=reqJson)
+            "{}{}".format(Constant.REAL_URL if type == "실전" else Constant.TEST_URL, KisATkit.tokenUrl), json=reqJson)
+
+        data = req.json()
 
         # 요청 오류시 예외 추가
         if (not req.ok):
             raise Exception(req.json())
 
-        data = req.json()
-
-        self.type = type
+        self.__type = type
         """모의투자, 실전 투자 여부"""
-        self.appKey = appKey
+        self.__appKey = appKey
         """appKey"""
-        self.appSecret = appSecret
+        self.__appSecret = appSecret
         """appSecret"""
-        self.account = account
+        self.__account = account
         """계좌 번호"""
-        self.HTS_Id = HTS_Id
+        self.__HTS_Id = HTS_Id
+        """HTS 계정 아이디"""
+        self.__tokenRes: dict = data
         """토큰 정보 딕셔너리"""
-        self.tokenRes: dict = data
 
-        def __refreshToken() -> None:
-            while True:
-                print("1초뒤 실행")
-                time.sleep(1)
+        # 토큰 관리 쓰레드 시작
+        Thread(target=self.__refreshToken).start()
+        time.sleep(1)
 
-                # url = "/oauth2/tokenP"
+    # 토큰 재발급 함수
+    def __refreshToken(self) -> None:
+        while True:
+            nextTime = self.__tokenRes["expires_in"] - 1000
+            time.sleep(nextTime)
+            url = KisATkit.tokenUrl
 
-                # reqJson = {
-                #     "grant_type": "client_credentials",
-                #     "appkey": self.appKey,
-                #     "appsecret": self.appSecret
-                # }
+            reqJson = {
+                "grant_type": "client_credentials",
+                "appkey": self.__appKey,
+                "appsecret": self.__appSecret
+            }
 
-                # req = requests.post(
-                #     "{}{}".format(Constant.REAL_URL if type == "실전" else Constant.TEST_URL, url), json=reqJson)
+            req = requests.post(
+                "{}{}".format(Constant.REAL_URL if type == "실전" else Constant.TEST_URL, url), json=reqJson)
 
-                # # 요청 오류시 예외 추가
-                # if (not req.ok):
-                #     raise Exception(req.json())
+            data = req.json()
 
-                # data = req.json()
+            # 요청 오류시 예외 추가
+            if (not req.ok):
+                raise Exception("토큰 재발급 실패: {}".format(data))
 
-                # # 요청 오류시 예외 추가
-                # if (not req.ok):
-                #     raise Exception(req.json())
-
-                # self.tokenRes = data
-
-        Thread(target=__refreshToken)
+            self.__tokenRes = data
